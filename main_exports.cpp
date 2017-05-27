9b95d5fd85eee8ea0b2ba8093b2b4ee7008680fe
paulhwang <phwang@gmail.com>
2017-05-27 10:55:38 +0000

    e

--- main_exports.cpp
+++ main_exports.cpp
@@ -4,7 +4,9 @@
   File name: main_exports.cpp
 */
 
+#include <string.h>
 #include "utils_dir/queue_mgr_class.h"
+#include "root_dir/base_mgr_dir/base_mgr_class.h"
 #include "engine_class.h"
 
 void mainReceiveDataFromTransport (void* main_object_val, void *data_val) {
@@ -17,6 +19,10 @@ void mainTransmitDataToTransport (void* main_object_val, void *data_val) {
 
 void EngineClass::exportRreceiveDataFromTransport (void *data_val) {
     this->goReceiveQueue()->enqueueData(data_val);
+    char *game_name = (char *)"go";
+    if (!strcmp(game_name, "go")) {
+    	this->goBaseMgrObject()->exportReceiveData(data_val);
+    }
 }
 
 void EngineClass::exportTransmitDataToTransport(void *data_val)
