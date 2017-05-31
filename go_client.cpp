5270cc4cc79c83879c5294f6dee4d6ab3476b708
paulhwang <phwang@gmail.com>
2017-05-31 06:33:09 +0000

    c

--- go_client.cpp
+++ go_client.cpp
@@ -8,11 +8,19 @@
 #include "./utils_dir/transport_class.h"
 #include "./utils_dir/logit.h"
 
+TransportClass *transport_object;
+
 void mainReceiveDataFromTransport (void* engine_object_val, void *data_val) {
     //printf("mainReceiveDataFromTransport() %s\n", (char *) data_val);
     char *data = (char *) data_val;
 
     if (*data == 'm') {
+        if (1) {
+            char s[LOGIT_BUF_SIZE];
+            sprintf(s, "base_id=%s", data);
+            LOGIT("mainReceiveDataFromTransport", s);
+        }
+        transport_object->exportTransmitData((void *)  "dMove   03021001");
 
     }
     else if (*data == 'd') {
@@ -21,8 +29,8 @@ void mainReceiveDataFromTransport (void* engine_object_val, void *data_val) {
 }
 
 int main (int argc, char** argv) {
-  	TransportClass *transport_object = new TransportClass(null);
-  	transport_object->clientThreadFunction(0, TRANSPORT_PORT_NUMBER_FOR_ME);
-  	transport_object->exportTransmitData((void *)  "dMove   03021001");
-  	sleep(1000);
+    transport_object = new TransportClass(null);
+    transport_object->clientThreadFunction(0, TRANSPORT_PORT_NUMBER_FOR_ME);
+    transport_object->exportTransmitData((void *)  "dMove   03021001");
+    sleep(1000);
 }
