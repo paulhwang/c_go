b123e80ec7bd6264a7d2e372fe6c9072f2f2fa18
paulhwang <phwang@gmail.com>
2017-05-31 05:51:53 +0000

    d

--- go_client.cpp
+++ go_client.cpp
@@ -16,6 +16,6 @@ void mainReceiveDataFromTransport (void* engine_object_val, void *data_val) {
 int main (int argc, char** argv) {
   	TransportClass *transport_object = new TransportClass(null);
   	transport_object->clientThreadFunction(0, TRANSPORT_PORT_NUMBER_FOR_ME);
-  	transport_object->exportTransmitData((void *)  "Move   03021001");
+  	transport_object->exportTransmitData((void *)  "dMove   03021001");
   	sleep(1000);
 }
--- root_dir/base_mgr_dir/base_mgr_exports.cpp
+++ root_dir/base_mgr_dir/base_mgr_exports.cpp
@@ -9,7 +9,15 @@
 
 void BaseMgrClass::exportReceiveData(void *data_val)
 {
-    this->receiveQueue->enqueueData(data_val);
+    char *data = (char *) data_val;
 
+    if (*data == 'm') {
+
+    }
+    else if (*data == 'd') {
+        this->receiveQueue->enqueueData(data + 1);
+    }
+    else {
+    }
 }
 
