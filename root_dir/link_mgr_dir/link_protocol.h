cedaf8ebfa8903579f23189526222bd8ce9f2623
paulhwang <phwang@gmail.com>
2017-06-01 13:33:15 +0000

    l

---
+++ root_dir/link_mgr_dir/link_protocol.h
@@ -0,0 +1,13 @@
+/*
+  Copyrights reserved
+  Written by Paul Hwang
+  File name: link_protocol.h
+*/
+
+#pragma once
+
+#define LINK_PROTOCOL_COMMAND_CREATE_LINK "C"
+#define LINK_PROTOCOL_COMMAND_DELETE_LINK "D"
+
+#define LINK_PROTOCOL_COMMAND_CREATE_SESSION "c"
+#define LINK_PROTOCOL_COMMAND_DELETE_SESSION "d"
