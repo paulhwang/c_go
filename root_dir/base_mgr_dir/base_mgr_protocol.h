c608b00f7a36d22e828104c007502c00dc3234f9
paulhwang <phwang@gmail.com>
2017-06-01 13:37:30 +0000

    b

---
+++ root_dir/base_mgr_dir/base_mgr_protocol.h
@@ -0,0 +1,10 @@
+/*
+  Copyrights reserved
+  Written by Paul Hwang
+  File name: base_mgr_protocol.h
+*/
+
+#pragma once
+
+#define BASE_MGR_PROTOCOL_COMMAND_CREATE_BASE "C"
+#define BASE_MGR_PROTOCOL_COMMAND_DELETE_BASE "D"
--- root_dir/link_mgr_dir/link_protocol.h
+++ root_dir/link_mgr_dir/link_protocol.h
@@ -1,13 +1,13 @@
 /*
   Copyrights reserved
   Written by Paul Hwang
-  File name: link_protocol.h
+  File name: link_mgr_protocol.h
 */
 
 #pragma once
 
-#define LINK_PROTOCOL_COMMAND_CREATE_LINK "C"
-#define LINK_PROTOCOL_COMMAND_DELETE_LINK "D"
+#define LINK_MGR_PROTOCOL_COMMAND_CREATE_LINK "C"
+#define LINK_MGR_PROTOCOL_COMMAND_DELETE_LINK "D"
 
-#define LINK_PROTOCOL_COMMAND_CREATE_SESSION "c"
-#define LINK_PROTOCOL_COMMAND_DELETE_SESSION "d"
+#define LINK_MGR_PROTOCOL_COMMAND_CREATE_SESSION "c"
+#define LINK_MGR_PROTOCOL_COMMAND_DELETE_SESSION "d"
