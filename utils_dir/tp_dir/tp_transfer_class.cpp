063b4fbffb2040a536a5f05cf65af2b7615ffe9f
paulhwang <phwang@gmail.com>
2017-06-04 08:54:38 +0000

    m

--- utils_dir/tp_dir/tp_transfer_class.cpp
+++ utils_dir/tp_dir/tp_transfer_class.cpp
@@ -11,10 +11,10 @@
 #include <unistd.h>
 #include <netdb.h>
 #include <pwd.h>
+#include "../../utils_dir/phwang.h"
+#include "../../utils_dir/queue_dir/queue_class.h"
 #include "tp_transfer_class.h"
 #include "tp_class.h"
-#include "../../utils_dir/logit_dir/logit.h"
-#include "../../utils_dir/queue_dir/queue_class.h"
 
 TpTransferClass::TpTransferClass (TpClass *tp_object_val, void (*receive_callback_val)(void *, void *), void *receive_object_val)
 {
