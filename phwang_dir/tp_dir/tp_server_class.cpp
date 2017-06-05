28edba11df35116c21bf8e871ca91d5f25b2b443
paulhwang <phwang@gmail.com>
2017-06-05 11:15:11 +0000

    t

--- phwang_dir/tp_dir/tp_server_class.cpp
+++ phwang_dir/tp_dir/tp_server_class.cpp
@@ -151,7 +151,7 @@ void TpServerClass::serverThreadFunction (unsigned short port_val,
     this->logit("startServer", "accepted");
 
 
-    TpTransferClass *tp_transfer_object = new TpTransferClass(this->theReceiveCallbackFunc, this->theAcceptCallbackParameter);
+    TpTransferClass *tp_transfer_object = new TpTransferClass(receive_callback_func_val, receive_callback_parameter_val);
     tp_transfer_object_val->startThreads(data_socket);
     //tp_transfer_object->startThreads(data_socket);
     accept_callback_func_val(accept_callback_parameter_val, tp_transfer_object_val);
