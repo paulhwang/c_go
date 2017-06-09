457c62bcb9dea95d7b6477d4d202d3e4951ed8d4
paulhwang <phwang@gmail.com>
2017-06-09 03:22:31 +0000

    t

---
+++ server_proj_dir/game_server_dir/game_server_class.cpp
@@ -0,0 +1,63 @@
+/*
+  Copyrights reserved
+  Written by Paul Hwang
+  File name: game_server_class.cpp
+*/
+
+#include "../../phwang_dir/phwang.h"
+#include "game_server_class.h"
+
+GameServerClass::GameServerClass (void *main_object_val)
+{
+    memset(this, 0, sizeof(GameServerClass));
+    this->theMainObject = main_object_val;
+    this->theGroupMgrObject = 0;
+
+    if (1) {
+        this->logit("GameServerClass", "init");
+    }
+}
+
+GameServerClass::~GameServerClass (void)
+{
+}
+
+void GameServerClass::insertGroup (GroupClass *group_object_val)
+{
+    int i = 0;
+    while (i < GAME_SERVER_GROUP_ARRAY_SIZE) {
+        if (!this->theGroupTableArray[i]) {
+            this->theGroupTableArray[i] = group_object_val;
+            return;
+        }
+        i++;
+    }
+    this->abend("insertGroup", "table is full");
+}
+
+void GameServerClass::removeGroup (GroupClass *group_object_val)
+{
+    int i = 0;
+    while (i < GAME_SERVER_GROUP_ARRAY_SIZE) {
+        if (this->theGroupTableArray[i] == group_object_val) {
+            this->theGroupTableArray[i] = 0;
+            return;
+        }
+        i++;
+    }
+    this->abend("removeGroup", "not found");
+}
+
+void GameServerClass::logit (char const* str0_val, char const* str1_val)
+{
+    char s[LOGIT_BUF_SIZE];
+    sprintf(s, "%s::%s", this->objectName(), str0_val);
+    //this->theGroupMgrObject->groupMgrLogit(s, str1_val);
+}
+
+void GameServerClass::abend (char const* str0_val, char const* str1_val)
+{
+    char s[LOGIT_BUF_SIZE];
+    sprintf(s, "%s::%s", this->objectName(), str0_val);
+    //this->theGroupMgrObject->groupMgrAbend(s, str1_val);
+}
---
+++ server_proj_dir/game_server_dir/game_server_class.h
@@ -0,0 +1,29 @@
+/*
+  Copyrights reserved
+  Written by Paul Hwang
+  File name: game_server_class.h
+*/
+
+#pragma once
+
+class GroupMgrClass;
+class GroupClass;
+
+class GameServerClass {
+#define GAME_SERVER_GROUP_ARRAY_SIZE 32
+
+    void *theMainObject;
+    GroupMgrClass *theGroupMgrObject;
+    GroupClass *theGroupTableArray[GAME_SERVER_GROUP_ARRAY_SIZE];
+
+    void logit(char const* str0_val, char const* str1_val);
+    void abend(char const* str0_val, char const* str1_val);
+
+public:
+    GameServerClass(void *main_object_val);
+    ~GameServerClass(void);
+    char const* objectName(void) {return "GameServerClass";}
+
+    void insertGroup(GroupClass *game_object_val);
+    void removeGroup(GroupClass *game_object_val);
+};
