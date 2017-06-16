3a51c70162d3cc4ba3e05e2472b2f5a2c27179b2
paulhwang <phwang@gmail.com>
2017-06-16 02:56:16 +0000

    t

--- phwang_dir/net_dir/tp_connect.cpp
+++ phwang_dir/net_dir/tp_connect.cpp
@@ -3,8 +3,3 @@
   Written by Paul Hwang
   File name: tp_connect.cpp
 */
-
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include "../../phwang_dir/phwang.h"
-#include "tp_transfer_class.h"
--- phwang_dir/phwang_class.cpp
+++ phwang_dir/phwang_class.cpp
@@ -9,7 +9,6 @@
 #include "phwang.h"
 #include "phwang_class.h"
 #include "queue_dir/queue_class.h"
-#include "net_dir/tp_connect.h"
 #include "net_dir/tp_server_class.h"
 #include "net_dir/tp_transfer_class.h"
 
