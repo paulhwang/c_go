44a4ea2b484e25c44e04507f4de401ff3aa22cd3
paulhwang <phwang@gmail.com>
2017-06-16 02:16:48 +0000

    t

--- phwang_dir/extern_dir/phwang_class.cpp
+++ phwang_dir/extern_dir/phwang_class.cpp
@@ -7,6 +7,9 @@
 #include "../phwang.h"
 #include "phwang_class.h"
 #include "../queue_dir/queue_class.h"
+#include "../net_dir/tp_connect.h"
+#include "../net_dir/tp_server_class.h"
+#include "../net_dir/tp_transfer_class.h"
 
 void PhwangClass::logit (char const* str0_val, char const* str1_val)
 {
@@ -141,3 +144,77 @@ void *PhwangClass::dequeue (void *queue_val)
 /**************************************************************************************************************/
 /**************************************************************************************************************/
 /**************************************************************************************************************/
+
+void *PhwangClass::mallocTpServer (
+                        void *caller_object_val,
+                        unsigned short port_val,
+                        void (*accept_callback_func_val)(void *, void *),
+                        void *accept_callback_parameter_val,
+                        void (*receive_callback_func_val)(void *, void *),
+                        void *receive_callback_parameter_val,
+                        char const *who_val)
+{
+    return new TpServerClass(
+                    caller_object_val,
+                    port_val,
+                    accept_callback_func_val,
+                    accept_callback_parameter_val,
+                    receive_callback_func_val,
+                    receive_callback_parameter_val,
+                    who_val);
+}
+
+void *PhwangClass::tpConnect (
+                    unsigned long ip_addr_val, 
+                    unsigned short port_val, 
+                    void (*receive_callback_val)(void *, void *), 
+                    void *receive_object_val,
+                    char const *who_val)
+{
+    return tpConnectServiceFunction(ip_addr_val, port_val, receive_callback_val, receive_object_val, who_val);
+}
+
+void PhwangClass::freeTpServer (void *tp_server_object_val)
+{
+    if (!tp_server_object_val) {
+        phwangAbend("phwangFreeTpTransfer", "null tp_server_object_val");
+        return;
+    }
+
+    if (strcmp(((TpServerClass *) tp_server_object_val)->objectName(), "TpServerClass")) {
+        phwangAbend("phwangFreeTpTransfer", "wrong object");
+        return;
+    }
+
+    ((TpServerClass *) tp_server_object_val)->~TpServerClass();
+}
+
+void PhwangClass::freeTpTransfer (void *tp_transfer_object_val)
+{
+    if (!tp_transfer_object_val) {
+        phwangAbend("phwangFreeTpTransfer", "null tp_transfer_object_val");
+        return;
+    }
+
+    if (strcmp(((TpTransferClass *) tp_transfer_object_val)->objectName(), "TpTransferClass")) {
+        phwangAbend("phwangFreeTpTransfer", "wrong object");
+        return;
+    }
+
+    ((TpTransferClass *) tp_transfer_object_val)->~TpTransferClass();
+}
+
+void PhwangClass::tpTransmit (void *tp_transfer_object_val, char *data_val)
+{
+    if (!tp_transfer_object_val) {
+        phwangAbend("phwangTpTransmit", "null tp_transfer_object_val");
+        return;
+    }
+
+    if (strcmp(((TpTransferClass *) tp_transfer_object_val)->objectName(), "TpTransferClass")) {
+        phwangAbend("phwangTpTransmit", "wrong object");
+        return;
+    }
+
+    ((TpTransferClass *) tp_transfer_object_val)->exportTransmitData(data_val);
+}
--- phwang_dir/extern_dir/phwang_queue.cpp
+++
@@ -1,9 +0,0 @@
-/*
-  Copyrights reserved
-  Written by Paul Hwang
-  File name: phwang_queue.cpp
-*/
-
-#include "../phwang.h"
-#include "../queue_dir/queue_class.h"
-#include "phwang_class.h"
