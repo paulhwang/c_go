126a2a6172619a36ed0e36c8a6d8212fdeb5cb7b
paulhwang <phwang@gmail.com>
2017-06-17 05:42:09 +0000

    t

--- server_proj_dir/fabric_dir/fabric_class.h
+++ server_proj_dir/fabric_dir/fabric_class.h
@@ -20,12 +20,20 @@ class FabricClass {
     void logit(char const* str0_val, char const* str1_val);
     void abend(char const* str0_val, char const* str1_val);
 
-public:
-    FabricClass(void);
-    ~FabricClass(void);
+protected:
+    friend class LinkMgrClass;
+    friend class SessionMgrClass;
+    friend class GroupMgrClass;
+    friend class UFabricClass;
+    friend class DFabricClass;
+
     char const* objectName(void) {return "FabricClass";}
     LinkMgrClass *linkMgrObject(void) {return this->theLinkMgrObject;}
     GroupMgrClass *groupMgrObject(void) {return this->theGroupMgrObject;}
     UFabricClass *uFabricObject(void) {return this->theUFabricObject;}
     DFabricClass *dFabricObject(void) {return this->theDFabricObject;}
+
+public:
+    FabricClass(void);
+    ~FabricClass(void);
 };
