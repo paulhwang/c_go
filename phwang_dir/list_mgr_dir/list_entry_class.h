47c35dcd8c324108e7172af140b6ddd83583cbc6
paulhwang <phwang@gmail.com>
2017-06-17 13:32:09 +0000

    t

--- phwang_dir/list_mgr_dir/list_entry_class.h
+++ phwang_dir/list_mgr_dir/list_entry_class.h
@@ -11,10 +11,14 @@ class ListMgrClass;
 class ListEntryClass {
     ListMgrClass *theListMgrObject;
     int theEntryId;
+    void *theEntryData;
 
     void debug(int on_off_val, char const* str0_val, char const* str1_val);
     void logit(char const* str0_val, char const* str1_val);
     void abend(char const* str0_val, char const* str1_val);
+
+protected:
+    void *entryData(void) {return this->theEntryData;}
 
 public:
     ListEntryClass(ListMgrClass *list_mgr_object, int entry_id_val);
