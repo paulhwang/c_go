2e2255c3e1f6f2318fbf38a3200500c0dc239c97
paulhwang <phwang@gmail.com>
2017-06-23 12:20:19 +0000

    t

--- server_proj_dir/engine_dir/base_mgr_dir/base_mgr_class.cpp
+++ server_proj_dir/engine_dir/base_mgr_dir/base_mgr_class.cpp
@@ -22,6 +22,47 @@ BaseMgrClass::~BaseMgrClass (void)
     phwangFreeTpTransfer(this->theTpTransferObject); 
 }
 
+int BaseMgrClass::allocBaseIndex (void)
+{
+    int index = 0;
+    while (index < BASE_MGR_BASE_ARRAY_SIZE) {
+        if (!this->theBaseTableArray[index]) {
+            return index;
+        }
+        index++;
+    }
+    return -1;
+}
+
+int BaseMgrClass::allocBaseId (void)
+{
+    if (this->theGlobalBaseId >= BASE_MGR_MAX_GLOBAL_BASE_ID) {
+        this->theGlobalBaseId = 0;
+    }
+    this->theGlobalBaseId++;
+    return this->theGlobalBaseId;
+}
+
+GoBaseClass *BaseMgrClass::mallocGoBase (void)
+{
+    int base_id = this->allocBaseId();
+    int base_index = this->allocBaseIndex();
+    if (base_index == -1) {
+        this->abend("mallocGoBase", "no space");
+        return 0;
+    }
+
+    GoBaseClass *base_object = new GoBaseClass(this, base_id, base_index);
+    this->theBaseTableArray[base_index] = base_object;
+
+    char *data_buf = (char *) malloc(BASE_MGR_DATA_BUFFER_SIZE + 4);
+    data_buf[0] = BASE_MGR_PROTOCOL_RESPOND_IS_MALLOC_BASE;
+    data_buf[1] = BASE_MGR_PROTOCOL_GAME_NAME_IS_GO;
+    phwangEncodeIdIndex(data_buf + 2, base_id, BASE_MGR_PROTOCOL_BASE_ID_SIZE, base_index, BASE_MGR_PROTOCOL_BASE_INDEX_SIZE);
+
+    return base_object;
+}
+
 GoBaseClass *BaseMgrClass::searchBase (char *data_val)
 {
     int base_id;
