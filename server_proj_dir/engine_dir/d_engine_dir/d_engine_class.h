3616dc31eafa103e899fbb11994c5f6f87ea25f1
paulhwang <phwang@gmail.com>
2017-06-23 09:27:47 +0000

    t

--- server_proj_dir/engine_dir/d_engine_dir/d_engine_class.h
+++ server_proj_dir/engine_dir/d_engine_dir/d_engine_class.h
@@ -7,6 +7,8 @@
 #pragma once
 
 class DEngineClass {
+    void startNetConnect(void);
+
     void debug(int on_off_val, char const* str0_val, char const* str1_val) {if (on_off_val) this->logit(str0_val, str1_val);};
     void logit(char const* str0_val, char const* str1_val);
     void abend(char const* str0_val, char const* str1_val);
@@ -15,4 +17,7 @@ public:
     DEngineClass(void);
     ~DEngineClass(void);
     char const* objectName(void) {return "DEngineClass";}
+
+    /* exports */
+    void exportedparseFunction(char *data_val);
 };
