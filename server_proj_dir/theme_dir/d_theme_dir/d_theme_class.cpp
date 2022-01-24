35842a08bda0e0427f1eab87dadad5b9315a8cd4
phwang <phwang@gmail.com>
2022-01-24 08:36:38 +0800

    debug

--- server_proj_dir/theme_dir/d_theme_dir/d_theme_class.cpp
+++ server_proj_dir/theme_dir/d_theme_dir/d_theme_class.cpp
@@ -23,7 +23,10 @@ DThemeClass::~DThemeClass (void)
 }
 
 void dThemeReceiveDataFromTransport (void *tp_transfer_object_val, void *game_d_server_object_val, void *data_val) {
-    phwangLogit("Golbal::dThemeReceiveDataFromTransport", (char *) data_val);
+    if (1) { /* debug */
+        phwangLogit("Golbal::dThemeReceiveDataFromTransport", (char *) data_val);
+    }
+
     ((DThemeClass *) game_d_server_object_val)->exportedparseFunction((char *) data_val);
     phwangFree(data_val, "dThemeReceiveDataFromTransport");
 }
