2a12534f5dd9847eba97e9b3c8a118ae69a66ce8
Paul Hwang <phwang@gmail.com>
2022-02-09 23:34:12 +0800

    db

--- phwang_dir/db_dir/db_root_class.cpp
+++ phwang_dir/db_dir/db_root_class.cpp
@@ -10,16 +10,24 @@
 
 DbRootClass::DbRootClass (void)
 {
-    this->createDbs();
+    this->theGoConnect = this->connectGoDb();
+    if (this->goConnect() == 0) {
+        this->abend("connectDbs", "fail to connnect to go_db");
+        return;
+    }
+
+    this->createTables();
+    this->insertAccount(0);
 }
 
 DbRootClass::~DbRootClass (void)
 {
+    PQfinish(this->goConnect());
 }
-
-void DbRootClass::createDbs (void)
-{
-    this->createLoginDb();
+ 
+void DbRootClass::createTables (void) {
+    this->createAccountTable();
+    this->createCarTable();
 }
 
 void do_exit0(PGconn *conn) {
@@ -35,18 +43,18 @@ void do_exit(PGconn *conn, PGresult *res) {
     PQfinish(conn);    
 }
 
-void DbRootClass::createLoginDb (void)
+PGconn *DbRootClass::connectGoDb (void)
 {
     int lib_ver = PQlibVersion();
 
     printf("Version of libpq: %d\n", lib_ver);
 
-    PGconn *conn = PQconnectdb("user=phwang dbname=login_db");
+    PGconn *conn = PQconnectdb("user=phwang dbname=go_db");
     if (PQstatus(conn) == CONNECTION_BAD) {
         
-        fprintf(stderr, "Connection to database failed: %s\n", PQerrorMessage(conn));
+        fprintf(stderr, "Connection to database failed: %s\n", PQerrorMessage(this->goConnect()));
         do_exit0(conn);
-        return;
+        return 0;
     }
 
     int ver = PQserverVersion(conn);
@@ -59,87 +67,143 @@ void DbRootClass::createLoginDb (void)
     printf("User: %s\n", user);
     printf("Database name: %s\n", db_name);
     printf("Password: %s\n", pswd);
-    
-    PGresult *res = PQexec(conn, "DROP TABLE IF EXISTS Cars");
+    return conn;
+}
+
+void DbRootClass::createAccountTable (void)
+{
+    this->logit("createAccountTable", "");
+    PGresult *res = PQexec(this->goConnect(), "DROP TABLE IF EXISTS Accounts");
     
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);
+        do_exit(this->goConnect(), res);
     }
     
     PQclear(res);
     
-    res = PQexec(conn, "CREATE TABLE Cars(Id INTEGER PRIMARY KEY," \
+    res = PQexec(this->goConnect(), "CREATE TABLE Accounts(Id INTEGER PRIMARY KEY," \
+        "Name VARCHAR(20), Password VARCHAR(20))");
+
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        do_exit(this->goConnect(), res); 
+    }
+    
+    PQclear(res);
+}
+
+
+void DbRootClass::insertAccount (char const *str_val) {
+    PGresult *res;
+
+    res = PQexec(this->goConnect(), "INSERT INTO Accounts VALUES(1, 'admin','pp123')");
+        
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) 
+        do_exit(this->goConnect(), res);     
+    
+    PQclear(res);    
+
+   res = PQexec(this->goConnect(), "INSERT INTO Accounts VALUES(2, 'phwang','p123')");
+        
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) 
+        do_exit(this->goConnect(), res);     
+    
+    PQclear(res);    
+}
+
+void DbRootClass::createCarTable(void) {
+    this->logit("createCarTable", "");
+
+    PGresult *res = PQexec(this->goConnect(), "DROP TABLE IF EXISTS Cars");
+    
+    if (PQresultStatus(res) != PGRES_COMMAND_OK) {
+        do_exit(this->goConnect(), res);
+    }
+    
+    PQclear(res);
+    
+    res = PQexec(this->goConnect(), "CREATE TABLE Cars(Id INTEGER PRIMARY KEY," \
         "Name VARCHAR(20), Price INT)");
         
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res); 
+        do_exit(this->goConnect(), res); 
     }
     
     PQclear(res);
     
-    res = PQexec(conn, "INSERT INTO Cars VALUES(1,'Audi',52642)");
+    res = PQexec(this->goConnect(), "INSERT INTO Cars VALUES(1,'Audi',52642)");
         
     if (PQresultStatus(res) != PGRES_COMMAND_OK) 
-        do_exit(conn, res);     
+        do_exit(this->goConnect(), res);     
     
     PQclear(res);    
     
-    res = PQexec(conn, "INSERT INTO Cars VALUES(2,'Mercedes',57127)");
+    res = PQexec(this->goConnect(), "INSERT INTO Cars VALUES(2,'Mercedes',57127)");
         
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);   
+        do_exit(this->goConnect(), res);   
     }
     
     PQclear(res);    
     
-    res = PQexec(conn, "INSERT INTO Cars VALUES(3,'Skoda',9000)");
+    res = PQexec(this->goConnect(), "INSERT INTO Cars VALUES(3,'Skoda',9000)");
         
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);   
+        do_exit(this->goConnect(), res);   
     }
     
     PQclear(res);  
     
-    res = PQexec(conn, "INSERT INTO Cars VALUES(4,'Volvo',29000)");
+    res = PQexec(this->goConnect(), "INSERT INTO Cars VALUES(4,'Volvo',29000)");
         
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);   
+        do_exit(this->goConnect(), res);   
     }
     
     PQclear(res);      
     
-    res = PQexec(conn, "INSERT INTO Cars VALUES(5,'Bentley',350000)");
+    res = PQexec(this->goConnect(), "INSERT INTO Cars VALUES(5,'Bentley',350000)");
         
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);   
+        do_exit(this->goConnect(), res);   
     }
     
     PQclear(res);  
     
-    res = PQexec(conn, "INSERT INTO Cars VALUES(6,'Citroen',21000)");
+    res = PQexec(this->goConnect(), "INSERT INTO Cars VALUES(6,'Citroen',21000)");
         
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);   
+        do_exit(this->goConnect(), res);   
     }
     
     PQclear(res);  
     
-    res = PQexec(conn, "INSERT INTO Cars VALUES(7,'Hummer',41400)");
+    res = PQexec(this->goConnect(), "INSERT INTO Cars VALUES(7,'Hummer',41400)");
         
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);   
+        do_exit(this->goConnect(), res);   
     }
     
     PQclear(res);  
     
-    res = PQexec(conn, "INSERT INTO Cars VALUES(8,'Volkswagen',21600)");
+    res = PQexec(this->goConnect(), "INSERT INTO Cars VALUES(8,'Volkswagen',21600)");
         
     if (PQresultStatus(res) != PGRES_COMMAND_OK) {
-        do_exit(conn, res);   
+        do_exit(this->goConnect(), res);   
     }
     
     PQclear(res);  
-    PQfinish(conn);
 }
 
+void DbRootClass::logit (char const* str0_val, char const* str1_val)
+{
+    char s[LOGIT_BUF_SIZE];
+    sprintf(s, "%s::%s", this->objectName(), str0_val);
+    phwangLogit(s, str1_val);
+}
 
+void DbRootClass::abend (char const* str0_val, char const* str1_val)
+{
+    char s[LOGIT_BUF_SIZE];
+    sprintf(s, "%s::%s", this->objectName(), str0_val);
+    phwangAbend(s, str1_val);
+}
--- phwang_dir/db_dir/db_root_class.h
+++ phwang_dir/db_dir/db_root_class.h
@@ -6,9 +6,17 @@
 
 #pragma once
 
+#include "/usr/include/postgresql/libpq-fe.h"
+
 class DbRootClass {
-    void createDbs(void);
-    void createLoginDb(void);
+    PGconn *theGoConnect;
+
+    PGconn *connectGoDb(void);
+    void createTables (void);
+    void insertAccount(char const *str_val);
+
+    void createAccountTable(void);
+    void createCarTable(void);
 
     void debug(int on_off_val, char const *str0_val, char const *str1_val) {if (on_off_val) this->logit(str0_val, str1_val);}
     void logit(char const *str0_val, char const *str1_val);
@@ -17,7 +25,10 @@ class DbRootClass {
 public:
     DbRootClass(void);
     ~DbRootClass(void);
+
     char const *objectName(void) {return "DbRootClass";}
+    PGconn *goConnect(void) {return this->theGoConnect;}
+
 
     /* exports */
 };
