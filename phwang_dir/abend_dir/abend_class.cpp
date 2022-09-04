a32aea666775fe04169040de1f2d68bfea7afe1e
Paul Hwang <phwang@gmail.com>
2022-09-04 00:13:17 -0700

    abend

--- phwang_dir/abend_dir/abend_class.cpp
+++ phwang_dir/abend_dir/abend_class.cpp
@@ -16,12 +16,44 @@ AbendClass::~AbendClass(void)
 {
 }
 
-void AbendClass::logit (char const* str0_val, char const* str1_val)
+void AbendClass::phwangLogit (char const *str0_val, char const *str1_val)
 {
-    phwangLogit(str0_val, str1_val);
+    printf("%s() %s\n", str0_val, str1_val);
 }
 
-void AbendClass::abend (char const* str0_val, char const* str1_val)
+void AbendClass::phwangLogit3 (char const *str0_val, char const *str1_val, char const *str2_val)
 {
-    phwangAbend(str0_val, str1_val);
+    printf("%s() %s {%s}\n", str0_val, str1_val, str2_val);
+}
+
+void AbendClass::phwangAbend (char const *str0_val, char const *str1_val)
+{
+    printf("*****abend*****%s() %s\n", str0_val, str1_val);
+    int *junk = 0;
+    *junk = 0;
+}
+
+void AbendClass::phwangAbend3 (char const *str0_val, char const *str1_val, char const *str2_val)
+{
+    printf("*****abend*****%s() %s, %s\n", str0_val, str1_val, str2_val);
+    int *junk = 0;
+    *junk = 0;
+}
+
+void AbendClass::logit (char const *str0_val, char const *str1_val) {
+    char s[LOGIT_BUF_SIZE];
+    sprintf(s, "%s::%s", this->objectName(), str0_val);
+    this->phwangLogit(s, str1_val);
+}
+
+void AbendClass::abend (char const *str0_val, char const *str1_val) {
+    char s[LOGIT_BUF_SIZE];
+    sprintf(s, "%s::%s", this->objectName(), str0_val);
+    this->phwangAbend(s, str1_val);
+}
+
+void AbendClass::abend3 (char const *str0_val, char const *str1_val, char const *str2_val) {
+    char s[LOGIT_BUF_SIZE];
+    sprintf(s, "%s::%s", this->objectName(), str0_val);
+    this->phwangAbend3(s, str1_val, str2_val);
 }
--- phwang_dir/abend_dir/abend_class.h
+++ phwang_dir/abend_dir/abend_class.h
@@ -1,7 +1,7 @@
 /*
   Copyrights reserved
   Written by Paul Hwang
-  File name: abemd_class.h
+  File name: abend_class.h
 */
 
 #pragma once
@@ -10,10 +10,18 @@ class AbendClass
 {
     void debug(int on_off_val, char const *str0_val, char const *str1_val) {if (on_off_val) this->logit(str0_val, str1_val);};
     void logit(char const *str0_val, char const *str1_val);
+    void logit3(char const *str0_val, char const *str1_val, char const *str2_val);
     void abend(char const *str0_val, char const *str1_val);
+    void abend3(char const *str0_val, char const *str1_val, char const *str2_val);
 
 public:
     AbendClass(void);
     ~AbendClass(void);
     char const *objectName(void) {return "AbendClass";}
+
+    void phwangDebug(int on_off_val, char const *str0_val, char const *str1_val) {if (on_off_val) this->phwangLogit(str0_val, str1_val);}
+    void phwangLogit(char const *str0_val, char const *str1_val);
+    void phwangLogit3(char const *str0_val, char const *str1_val, char const *str2_val);
+    void phwangAbend(char const *str0_val, char const *str1_val);
+    void phwangAbend3(char const *str0_val, char const *str1_val, char const *str2_val);
 };
