ecea344af38026368e9cc3fadb419b7233118ce5
Paul Hwang <phwang@gmail.com>
2022-09-04 23:18:54 -0700

    dp

--- phwang_dir/malloc_dir/malloc_class.h
+++ phwang_dir/malloc_dir/malloc_class.h
@@ -28,9 +28,28 @@ public:
     int const static TP_RECEIVE2  = 2;
     int const static ENCODE_STRING  = 3;
     int const static DECODE_STRING  = 4;
+    int const static BAD_LINK  = 5;
+    int const static BAD_SESSION  = 6;
     int const static SIGN_UP  = 5;
     int const static SIGN_IN  = 6;
     int const static DATAGRAM  = 7;
+    int const static FREE_LINK  = 6;
+    int const static FREE_LINK_ERROR  = 7;
+    int const static GET_LINK_DATA  = 6;
+    int const static GET_LINK_DATA_ERROR  = 7;
+    int const static GET_NAME_LIST  = 6;
+    int const static GET_NAME_LIST_ERROR  = 7;
+    int const static SETUP_SESSION  = 7;
+    int const static SETUP_SESSION_ERROR  = 7;
+    int const static SETUP_SESSION2  = 7;
+    int const static SETUP_SESSION2_ERROR  = 7;
+    int const static SETUP_SESSION3  = 7;
+    int const static PUT_SESSION_DATA0  = 7;
+    int const static PUT_SESSION_DATA1  = 7;
+    int const static PUT_SESSION_DATA_ERROR  = 7;
+    int const static GET_SESSION_DATA  = 7;
+    int const static GET_SESSION_DATA_ERROR  = 7;
+    int const static MALLOC_ROOM  = 7;
     int const static DB_ACCOUNT_DEFAULT_NAME = 8;
 
     void *phwangMalloc1(int size_val, int who_val);
