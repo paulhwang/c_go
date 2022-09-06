9bdbfb7c0f2229eb7d652ddff003cd06420971ec
Paul Hwang <phwang@gmail.com>
2022-09-05 23:52:34 -0700

    dt

--- server_proj_dir/theme_dir/d_theme_dir/d_theme_class.cpp
+++ server_proj_dir/theme_dir/d_theme_dir/d_theme_class.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "../../../phwang_dir/phwang.h"
+#include "../../../phwang_dir/abend_dir/abend_class.h"
 #include "../../protocol_dir/net_port_protocol.h"
 #include "d_theme_class.h"
 
@@ -39,16 +40,76 @@ void DThemeClass::startNetConnect (void)
     this->theTpTransferObject = phwangTpConnect(0, FABRIC_THEME_PROTOCOL_TRANSPORT_PORT_NUMBER, dThemeReceiveDataFromTransport, this, this->objectName());
 }
 
-void DThemeClass::logit (char const* str0_val, char const* str1_val)
+void DThemeClass::debug (int debug_on_val, char const *func_name_val, char const *str1_val)
 {
-    char s[LOGIT_BUF_SIZE];
-    sprintf(s, "%s::%s", this->objectName(), str0_val);
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        phwangComposeFuncName(s, func_name_val);
+        phwangDebug(debug_on_val, s, str1_val);
+    }
+}
+
+void DThemeClass::debug2 (int debug_on_val, char const *func_name_val, char const *str1_val, char const *str2_val)
+{
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        phwangComposeFuncName(s, func_name_val);
+        phwangDebug2(debug_on_val, s, str1_val, str2_val);
+    }
+}
+
+void DThemeClass::debugInt(int debug_on_val, char const *func_name_val, char const *str1_val, int int1_val)
+{
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        phwangComposeFuncName(s, func_name_val);
+        phwangDebugInt(debug_on_val, s, str1_val, int1_val);
+    }
+}
+
+void DThemeClass::debugInt2(int debug_on_val, char const *func_name_val, char const *str1_val, int int1_val, char const *str2_val, int int2_val)
+{
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        phwangComposeFuncName(s, func_name_val);
+        phwangDebugInt2(debug_on_val, s, str1_val, int1_val, str2_val, int2_val);
+    }
+}
+
+void DThemeClass::logit (char const *func_name_val, char const *str1_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
     phwangLogit(s, str1_val);
 }
 
-void DThemeClass::abend (char const* str0_val, char const* str1_val)
+void DThemeClass::logit2 (char const *func_name_val, char const *str1_val, char const *str2_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
+    phwangLogit2(s, str1_val, str2_val);
+}
+
+void DThemeClass::logitInt(char const *func_name_val, char const *str1_val, int int1_val)
 {
-    char s[LOGIT_BUF_SIZE];
-    sprintf(s, "%s::%s", this->objectName(), str0_val);
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
+    phwangLogitInt(s, str1_val, int1_val);
+}
+
+void DThemeClass::logitInt2(char const *func_name_val, char const *str1_val, int int1_val, char const *str2_val, int int2_val)
+{
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
+    phwangLogitInt2(s, str1_val, int1_val, str2_val, int2_val);
+}
+
+void DThemeClass::abend (char const *func_name_val, char const *str1_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
     phwangAbend(s, str1_val);
+}
+
+void DThemeClass::abend2 (char const *func_name_val, char const *str1_val, char const *str2_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
+    phwangAbend2(s, str1_val, str2_val);
 }
