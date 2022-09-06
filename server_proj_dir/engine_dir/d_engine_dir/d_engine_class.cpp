758932215e78d3c05dee9ad066c8185896b58c1e
Paul Hwang <phwang@gmail.com>
2022-09-05 23:58:40 -0700

    de

--- server_proj_dir/engine_dir/d_engine_dir/d_engine_class.cpp
+++ server_proj_dir/engine_dir/d_engine_dir/d_engine_class.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "../../../phwang_dir/phwang.h"
+#include "../../../phwang_dir/abend_dir/abend_class.h"
 #include "../../protocol_dir/net_port_protocol.h"
 #include "d_engine_class.h"
 
@@ -36,16 +37,76 @@ void DEngineClass::startNetConnect (void)
     this->theTpTransferObject = phwangTpConnect(0, THEME_ENGINE_PROTOCOL_TRANSPORT_PORT_NUMBER, dEngineReceiveDataFromTransport, this, this->objectName());
 }
 
-void DEngineClass::logit (char const* str0_val, char const* str1_val)
+void DEngineClass::debug (int debug_on_val, char const *func_name_val, char const *str1_val)
 {
-    char s[LOGIT_BUF_SIZE];
-    sprintf(s, "%s::%s", this->objectName(), str0_val);
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        phwangComposeFuncName(s, func_name_val);
+        phwangDebug(debug_on_val, s, str1_val);
+    }
+}
+
+void DEngineClass::debug2 (int debug_on_val, char const *func_name_val, char const *str1_val, char const *str2_val)
+{
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        phwangComposeFuncName(s, func_name_val);
+        phwangDebug2(debug_on_val, s, str1_val, str2_val);
+    }
+}
+
+void DEngineClass::debugInt(int debug_on_val, char const *func_name_val, char const *str1_val, int int1_val)
+{
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        phwangComposeFuncName(s, func_name_val);
+        phwangDebugInt(debug_on_val, s, str1_val, int1_val);
+    }
+}
+
+void DEngineClass::debugInt2(int debug_on_val, char const *func_name_val, char const *str1_val, int int1_val, char const *str2_val, int int2_val)
+{
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        phwangComposeFuncName(s, func_name_val);
+        phwangDebugInt2(debug_on_val, s, str1_val, int1_val, str2_val, int2_val);
+    }
+}
+
+void DEngineClass::logit (char const *func_name_val, char const *str1_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
     phwangLogit(s, str1_val);
 }
 
-void DEngineClass::abend (char const* str0_val, char const* str1_val)
+void DEngineClass::logit2 (char const *func_name_val, char const *str1_val, char const *str2_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
+    phwangLogit2(s, str1_val, str2_val);
+}
+
+void DEngineClass::logitInt(char const *func_name_val, char const *str1_val, int int1_val)
 {
-    char s[LOGIT_BUF_SIZE];
-    sprintf(s, "%s::%s", this->objectName(), str0_val);
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
+    phwangLogitInt(s, str1_val, int1_val);
+}
+
+void DEngineClass::logitInt2(char const *func_name_val, char const *str1_val, int int1_val, char const *str2_val, int int2_val)
+{
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
+    phwangLogitInt2(s, str1_val, int1_val, str2_val, int2_val);
+}
+
+void DEngineClass::abend (char const *func_name_val, char const *str1_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
     phwangAbend(s, str1_val);
+}
+
+void DEngineClass::abend2 (char const *func_name_val, char const *str1_val, char const *str2_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
+    phwangAbend2(s, str1_val, str2_val);
 }
