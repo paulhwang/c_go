c93cbd66f40aa65a2e91e07efdb4c10705df0019
Paul Hwang <phwang@gmail.com>
2022-09-05 23:44:23 -0700

    uf

--- server_proj_dir/fabric_dir/u_fabric_dir/u_fabric_class.cpp
+++ server_proj_dir/fabric_dir/u_fabric_dir/u_fabric_class.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "../../../phwang_dir/phwang.h"
+#include "../../../phwang_dir/abend_dir/abend_class.h"
 #include "../../protocol_dir/net_port_protocol.h"
 #include "u_fabric_class.h"
 
@@ -50,16 +51,76 @@ void UFabricClass::startNetServer (void)
     this->theTpServerObject = phwangMallocTpServer(this, FABRIC_THEME_PROTOCOL_TRANSPORT_PORT_NUMBER, uFabricTpServerAcceptFunction, this, uFabricTpReceiveDataFunction, this, this->objectName());
 }
 
-void UFabricClass::logit (char const* str0_val, char const* str1_val)
+void UFabricClass::debug (int debug_on_val, char const *func_name_val, char const *str1_val)
 {
-    char s[LOGIT_BUF_SIZE];
-    sprintf(s, "%s::%s", this->objectName(), str0_val);
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        phwangComposeFuncName(s, func_name_val);
+        phwangDebug(debug_on_val, s, str1_val);
+    }
+}
+
+void UFabricClass::debug2 (int debug_on_val, char const *func_name_val, char const *str1_val, char const *str2_val)
+{
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        phwangComposeFuncName(s, func_name_val);
+        phwangDebug2(debug_on_val, s, str1_val, str2_val);
+    }
+}
+
+void UFabricClass::debugInt(int debug_on_val, char const *func_name_val, char const *str1_val, int int1_val)
+{
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        phwangComposeFuncName(s, func_name_val);
+        phwangDebugInt(debug_on_val, s, str1_val, int1_val);
+    }
+}
+
+void UFabricClass::debugInt2(int debug_on_val, char const *func_name_val, char const *str1_val, int int1_val, char const *str2_val, int int2_val)
+{
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        phwangComposeFuncName(s, func_name_val);
+        phwangDebugInt2(debug_on_val, s, str1_val, int1_val, str2_val, int2_val);
+    }
+}
+
+void UFabricClass::logit (char const *func_name_val, char const *str1_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
     phwangLogit(s, str1_val);
 }
 
-void UFabricClass::abend (char const* str0_val, char const* str1_val)
+void UFabricClass::logit2 (char const *func_name_val, char const *str1_val, char const *str2_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
+    phwangLogit2(s, str1_val, str2_val);
+}
+
+void UFabricClass::logitInt(char const *func_name_val, char const *str1_val, int int1_val)
 {
-    char s[LOGIT_BUF_SIZE];
-    sprintf(s, "%s::%s", this->objectName(), str0_val);
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
+    phwangLogitInt(s, str1_val, int1_val);
+}
+
+void UFabricClass::logitInt2(char const *func_name_val, char const *str1_val, int int1_val, char const *str2_val, int int2_val)
+{
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
+    phwangLogitInt2(s, str1_val, int1_val, str2_val, int2_val);
+}
+
+void UFabricClass::abend (char const *func_name_val, char const *str1_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
     phwangAbend(s, str1_val);
+}
+
+void UFabricClass::abend2 (char const *func_name_val, char const *str1_val, char const *str2_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    phwangComposeFuncName(s, func_name_val);
+    phwangAbend2(s, str1_val, str2_val);
 }
