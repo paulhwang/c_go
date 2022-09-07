f3682500761dfeb53f1d6418f7a561e0b09d6f4c
Paul Hwang <phwang@gmail.com>
2022-09-07 09:11:15 -0700

    array

--- phwang_dir/array_mgr_dir/array_mgr_class.cpp
+++ phwang_dir/array_mgr_dir/array_mgr_class.cpp
@@ -5,6 +5,7 @@
 */
 
 #include "../../phwang_dir/phwang.h"
+#include "../../phwang_dir/abend_dir/abend_class.h"
 #include "array_mgr_class.h"
 
 ArrayMgrClass::ArrayMgrClass(char const *who_val, char array_type_val, int max_array_size_val):
@@ -128,16 +129,76 @@ void ArrayMgrClass::removePointerElement(void *element_val)
     this->abend("removePointerElement", "not found");
 }
 
-void ArrayMgrClass::logit (char const* str0_val, char const* str1_val)
+void ArrayMgrClass::debug (int debug_on_val, char const *func_name_val, char const *str1_val)
 {
-    char s[LOGIT_BUF_SIZE];
-    sprintf(s, "%s(%s)::%s", this->objectName(), this->theWho, str0_val);
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        this->composeFuncNameExtra(s, func_name_val);
+        phwangDebug(debug_on_val, s, str1_val);
+    }
+}
+
+void ArrayMgrClass::debug2 (int debug_on_val, char const *func_name_val, char const *str1_val, char const *str2_val)
+{
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        this->composeFuncNameExtra(s, func_name_val);
+        phwangDebug2(debug_on_val, s, str1_val, str2_val);
+    }
+}
+
+void ArrayMgrClass::debugInt(int debug_on_val, char const *func_name_val, char const *str1_val, int int1_val)
+{
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        this->composeFuncNameExtra(s, func_name_val);
+        phwangDebugInt(debug_on_val, s, str1_val, int1_val);
+    }
+}
+
+void ArrayMgrClass::debugInt2(int debug_on_val, char const *func_name_val, char const *str1_val, int int1_val, char const *str2_val, int int2_val)
+{
+    if (debug_on_val) {
+        char s[AbendClass::LogitFuncNameBufSize];
+        this->composeFuncNameExtra(s, func_name_val);
+        phwangDebugInt2(debug_on_val, s, str1_val, int1_val, str2_val, int2_val);
+    }
+}
+
+void ArrayMgrClass::logit (char const *func_name_val, char const *str1_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    this->composeFuncNameExtra(s, func_name_val);
     phwangLogit(s, str1_val);
 }
 
-void ArrayMgrClass::abend (char const* str0_val, char const* str1_val)
+void ArrayMgrClass::logit2 (char const *func_name_val, char const *str1_val, char const *str2_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    this->composeFuncNameExtra(s, func_name_val);
+    phwangLogit2(s, str1_val, str2_val);
+}
+
+void ArrayMgrClass::logitInt(char const *func_name_val, char const *str1_val, int int1_val)
 {
-    char s[LOGIT_BUF_SIZE];
-    sprintf(s, "%s(%s)::%s", this->objectName(), this->theWho, str0_val);
+    char s[AbendClass::LogitFuncNameBufSize];
+    this->composeFuncNameExtra(s, func_name_val);
+    phwangLogitInt(s, str1_val, int1_val);
+}
+
+void ArrayMgrClass::logitInt2(char const *func_name_val, char const *str1_val, int int1_val, char const *str2_val, int int2_val)
+{
+    char s[AbendClass::LogitFuncNameBufSize];
+    this->composeFuncNameExtra(s, func_name_val);
+    phwangLogitInt2(s, str1_val, int1_val, str2_val, int2_val);
+}
+
+void ArrayMgrClass::abend (char const *func_name_val, char const *str1_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    this->composeFuncNameExtra(s, func_name_val);
     phwangAbend(s, str1_val);
+}
+
+void ArrayMgrClass::abend2 (char const *func_name_val, char const *str1_val, char const *str2_val) {
+    char s[AbendClass::LogitFuncNameBufSize];
+    this->composeFuncNameExtra(s, func_name_val);
+    phwangAbend2(s, str1_val, str2_val);
 }
--- phwang_dir/array_mgr_dir/array_mgr_class.h
+++ phwang_dir/array_mgr_dir/array_mgr_class.h
@@ -17,9 +17,17 @@ class ArrayMgrClass {
     void insertPointerElement(void *element_val);
     void removePointerElement(void *element_val);
 
-    void debug(int on_off_val, char const *str0_val, char const *str1_val) {if (on_off_val) this->logit(str0_val, str1_val);};
-    void logit(char const *str0_val, char const *str1_val);
-    void abend(char const *str0_val, char const *str1_val);
+    void debug(int debug_on_val, char const *func_name_val, char const *str1_val);
+    void debug2(int debug_on_val, char const *func_name_val, char const *str1_val, char const *str2_val);
+    void debugInt(int debug_on_val, char const *func_name_val, char const *str1_val, int int1_val);
+    void debugInt2(int debug_on_val, char const *func_name_val, char const *str1_val, int int1_val, char const *str2_val, int int2_val);
+    void logit(char const *func_name_val, char const *str1_val);
+    void logit2(char const *func_name_val, char const *str1_val, char const *str2_val);
+    void logitInt(char const *func_name_val, char const *str1_val, int int1_val);
+    void logitInt2(char const *func_name_val, char const *str1_val, int int1_val, char const *str2_val, int int2_val);
+    void abend(char const *func_name_val, char const *str1_val);
+    void abend2(char const *func_name_val, char const *str1_val, char const *str2_val);
+    void composeFuncNameExtra(char *buf_val, char const *func_name_val) {phwangComposeFuncNameExtra(buf_val, this->objectName(), func_name_val, this->theWho);}
 
 public:
     ArrayMgrClass(char const *who_val, char array_type_val, int max_array_size_val);
