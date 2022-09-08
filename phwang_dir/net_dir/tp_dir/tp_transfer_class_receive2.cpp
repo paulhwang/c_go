87b5d50c31e4d3a071988c3d56b8d73766a4ca41
Paul Hwang <phwang@gmail.com>
2022-09-07 21:38:52 -0700

    rcv

---
+++ phwang_dir/net_dir/tp_dir/tp_transfer_class_receive2.cpp
@@ -0,0 +1,79 @@
+/*
+  Copyrights reserved
+  Written by Paul Hwang
+  File name: tp_transfer_receive2.cpp
+*/
+
+#include <stdlib.h>
+#include <pthread.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <pwd.h>
+#include "../../../phwang_dir/phwang.h"
+#include "../../../phwang_dir/malloc_dir/malloc_class.h"
+#include "tp_transfer_class.h"
+
+void *TpTransferClass::receiveThreadFunction2 (void)
+{
+    char const *func_name_ = "receiveThreadFunction2";
+
+    while (1) {
+        char *raw_data = (char *) phwangDequeue(this->theReceiveQueue, "TpTransferClass::receiveThreadFunction2()");
+        if (raw_data) {
+            int raw_length = strlen(raw_data);
+            int length;
+            int length1;
+            char *data;
+
+            if (0) { /* debug */
+                char s[128];
+                sprintf(s, "(%s) data=%s", this->theWho, raw_data);
+                this->logit(func_name_, s);
+            }
+
+            if (raw_data[0] == '{') {
+                length = raw_length - (1 + TpTransferClass::DATA_LENGTH_SIZE + 1);
+                length1 = phwangDecodeNumber(&raw_data[1], TpTransferClass::DATA_LENGTH_SIZE);
+                if (length != length1) {
+                    char s[128];
+                    sprintf(s, "(%s) length not match!!! data=%s len=%d %d", this->theWho, raw_data, length, length1);
+                    this->abend(func_name_, s);
+                }
+
+                data = (char *) phwangMalloc(length + 32, MallocClass::receiveThreadFunction2);
+                memcpy(data, &raw_data[1 + TpTransferClass::DATA_LENGTH_SIZE], length);
+                data[length] = 0;
+             }
+            else {
+                if (1) { /* debug */
+                    char s[2000];
+                    sprintf(s, "(%s) data=%s len=%lu", this->theWho, raw_data, strlen(raw_data));
+                    this->logit(func_name_, s);
+                }
+                this->abend("receiveThreadFunction2: wrong header", raw_data);
+                continue;
+            }
+
+            phwangFree(raw_data);
+            this->receiveCallback()(this, this->theReceiveObject, data);
+        }
+    }
+    return 0;
+}
+
+void *tpTranferReceiveThreadFunction2 (void *this_val)
+{
+    return ((TpTransferClass *)this_val)->receiveThreadFunction2();
+}
+
+void TpTransferClass::startReceiveThread2 (void)
+{
+    this->debug(false, "startReceiveThread2", "create receiveThread");
+
+    int r = phwangPthreadCreate(&this->theReceiveThread2, NULL, tpTranferReceiveThreadFunction2, this);
+    if (r) {
+        printf("Error - pthread_create() return code: %d\n", r);
+        return;
+    }
+}
