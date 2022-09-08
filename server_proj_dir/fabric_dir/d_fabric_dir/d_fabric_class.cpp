b9d9845239247e330422acca41e0d27906231b23
Paul Hwang <phwang@gmail.com>
2022-09-07 22:05:40 -0700

    w

--- server_proj_dir/fabric_dir/d_fabric_dir/d_fabric_class.cpp
+++ server_proj_dir/fabric_dir/d_fabric_dir/d_fabric_class.cpp
@@ -43,7 +43,8 @@ void DFabricClass::exportedNetAcceptFunction (void *tp_transfer_object_val)
 }
 
 void dFabricTpReceiveDataFunction (void *tp_transfer_object_val, void *d_fabric_object_val, void *data_val) {
-    if (*((char *)(data_val + 1)) != FECommandClass::GET_LINK_DATA_COMMAND) {
+    char *data_str_val = (char *) data_val;
+    if (data_str_val[1] != FECommandClass::GET_LINK_DATA_COMMAND) {
         phwangDebugInt2(false,"Golbal::dFabricTpReceiveDataFunction", (char *) data_val, 99999, "index", ((TpTransferClass *) tp_transfer_object_val)->index());
     }
 
