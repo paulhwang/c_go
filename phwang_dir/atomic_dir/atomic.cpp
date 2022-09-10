c1ce29a4acce1ee82f411b1d28b243116c466436
Paul Hwang <phwang@gmail.com>
2022-09-10 09:02:31 -0700

    abend

---
+++ phwang_dir/atomic_dir/atomic.cpp
@@ -0,0 +1,26 @@
+/*
+  Copyrights reserved
+  Written by Paul Hwang
+  File name: atomic.c
+*/
+
+#pragma once
+#include "../phwang.h"
+
+void phwangIncrementAtomicCount (std::atomic<int> *count_val, char const *who_val, int max_count_val)
+{
+    (*count_val)++;
+    if (*count_val > max_count_val) {
+        printf("phwangIncrementAtomicCount(), count=%d\n", (*count_val).load());
+        phwangAbendS("phwangIncrementAtomicCount", who_val);
+    }
+}
+
+void phwangDecrementAtomicCount (std::atomic<int> *count_val, char const *who_val)
+{
+    (*count_val)--;
+    if (*count_val < 0) {
+        printf("phwangDecrementAtomicCount(), count=%d\n", (*count_val).load());
+        phwangAbendS("phwangDecrementAtomicCount", who_val);
+    }
+}
