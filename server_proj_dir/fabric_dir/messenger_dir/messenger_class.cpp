aaf36f1b83663c46859eab1bb2bd8d3b99ec635d
Paul Hwang <phwang@gmail.com>
2022-09-09 21:00:58 -0700

    m

--- server_proj_dir/fabric_dir/messenger_dir/messenger_class.cpp
+++ server_proj_dir/fabric_dir/messenger_dir/messenger_class.cpp
@@ -14,7 +14,7 @@ MessengerClass::MessengerClass (FabricClass *fabric_object_val)
 {
     memset(this, 0, sizeof(MessengerClass));
 
-    this->debug(false, "MessengerClass", "init");
+    phwangDebugS(false, "MessengerClass::MessengerClass", "init");
 }
 
 MessengerClass::~MessengerClass (void)
@@ -23,92 +23,18 @@ MessengerClass::~MessengerClass (void)
 
 void MessengerClass::initMessenger (void)
 {
-    this->debug(true, "initMessenger", "init");
+    phwangDebugS(true, "MessengerClass::initMessenger", "init");
     this->theMmwObject = new MmwClass(this->fabricObject());
 }
 
 char *MessengerClass::getMessage (void)
 {
     char *data = this->theMmwObject->readInputData();
-    this->debug(true, "getMessage", data);
+    phwangDebugS(true, "MessengerClass::getMessage", data);
     return data;
 }
 
 void MessengerClass::putMessage (char const *info_val)
 {
-    this->debug2(true, "putMessage", "data=", info_val);
-}
-
-void MessengerClass::debug (int debug_on_val, char const *func_name_val, char const *str1_val)
-{
-    if (debug_on_val) {
-        char s[AbendClass::LogitFuncNameBufSize];
-        phwangComposeFuncName(s, this->objectName(), func_name_val);
-        phwangDebug(debug_on_val, s, str1_val);
-    }
-}
-
-void MessengerClass::debug2 (int debug_on_val, char const *func_name_val, char const *str1_val, char const *str2_val)
-{
-    if (debug_on_val) {
-        char s[AbendClass::LogitFuncNameBufSize];
-        phwangComposeFuncName(s, this->objectName(), func_name_val);
-        phwangDebug2(debug_on_val, s, str1_val, str2_val);
-    }
-}
-
-void MessengerClass::debugInt(int debug_on_val, char const *func_name_val, char const *str1_val, int int1_val)
-{
-    if (debug_on_val) {
-        char s[AbendClass::LogitFuncNameBufSize];
-        phwangComposeFuncName(s, this->objectName(), func_name_val);
-        phwangDebugInt(debug_on_val, s, str1_val, int1_val);
-    }
-}
-
-void MessengerClass::debugInt2(int debug_on_val, char const *func_name_val, char const *str1_val, int int1_val, char const *str2_val, int int2_val)
-{
-    if (debug_on_val) {
-        char s[AbendClass::LogitFuncNameBufSize];
-        phwangComposeFuncName(s, this->objectName(), func_name_val);
-        phwangDebugInt2(debug_on_val, s, str1_val, int1_val, str2_val, int2_val);
-    }
-}
-
-void MessengerClass::logit (char const *func_name_val, char const *str1_val) {
-    char s[AbendClass::LogitFuncNameBufSize];
-    phwangComposeFuncName(s, this->objectName(), func_name_val);
-    phwangLogit(s, str1_val);
-}
-
-void MessengerClass::logit2 (char const *func_name_val, char const *str1_val, char const *str2_val) {
-    char s[AbendClass::LogitFuncNameBufSize];
-    phwangComposeFuncName(s, this->objectName(), func_name_val);
-    phwangLogit2(s, str1_val, str2_val);
-}
-
-void MessengerClass::logitInt(char const *func_name_val, char const *str1_val, int int1_val)
-{
-    char s[AbendClass::LogitFuncNameBufSize];
-    phwangComposeFuncName(s, this->objectName(), func_name_val);
-    phwangLogitInt(s, str1_val, int1_val);
-}
-
-void MessengerClass::logitInt2(char const *func_name_val, char const *str1_val, int int1_val, char const *str2_val, int int2_val)
-{
-    char s[AbendClass::LogitFuncNameBufSize];
-    phwangComposeFuncName(s, this->objectName(), func_name_val);
-    phwangLogitInt2(s, str1_val, int1_val, str2_val, int2_val);
-}
-
-void MessengerClass::abend (char const *func_name_val, char const *str1_val) {
-    char s[AbendClass::LogitFuncNameBufSize];
-    phwangComposeFuncName(s, this->objectName(), func_name_val);
-    phwangAbend(s, str1_val);
-}
-
-void MessengerClass::abend2 (char const *func_name_val, char const *str1_val, char const *str2_val) {
-    char s[AbendClass::LogitFuncNameBufSize];
-    phwangComposeFuncName(s, this->objectName(), func_name_val);
-    phwangAbend2(s, str1_val, str2_val);
+    phwangDebugSS(true, "MessengerClass::putMessage", "data=", info_val);
 }
