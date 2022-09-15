d1cde43afbf73c9f94d177b70ff34a381d5b0667
Paul Hwang <phwang@gmail.com>
2022-09-14 21:46:22 -0700

    e

--- server_proj_dir/engine_dir/d_engine_dir/d_engine_parse.cpp
+++ server_proj_dir/engine_dir/d_engine_dir/d_engine_parse.cpp
@@ -35,9 +35,6 @@ void DEngineClass::processSetupBase (char const *data_val)
 
     char const *room_id_index_val = data_val;
 
-    char *downlink_data;
-    char *data_ptr;
-
     GoBaseClass *go_base_object = this->theEngineObject->mallocGoBase(room_id_index_val + FT_CommandClass::ROOM_ID_INDEX_SIZE);
     if (!go_base_object) {
         phwangAbendS("DEngineClass::processSetupBase", "null go_base");
@@ -46,15 +43,16 @@ void DEngineClass::processSetupBase (char const *data_val)
     go_base_object->setRoomIdIndex(room_id_index_val);
 
     /* downlink */
-    downlink_data = data_ptr = (char *) phwangMalloc(TE_CommandClass::TE_DL_DATA_BUF_SIZE + 4, MallocClass::DENGINE_SETUP_BASE);
-    *data_ptr++ = TE_CommandClass::SETUP_BASE_RESPONSE;
+    char *current_ptr;
+    char *downlink_data = current_ptr = (char *) phwangMalloc(TE_CommandClass::TE_DL_DATA_BUF_SIZE + 4, MallocClass::DENGINE_SETUP_BASE);
+    *current_ptr++ = TE_CommandClass::SETUP_BASE_RESPONSE;
 
-    memcpy(data_ptr, room_id_index_val, FT_CommandClass::ROOM_ID_INDEX_SIZE);
-    data_ptr += FT_CommandClass::ROOM_ID_INDEX_SIZE;
+    memcpy(current_ptr, room_id_index_val, FT_CommandClass::ROOM_ID_INDEX_SIZE);
+    current_ptr += FT_CommandClass::ROOM_ID_INDEX_SIZE;
 
-    memcpy(data_ptr, go_base_object->goBaseIdIndex(), TE_CommandClass::BASE_ID_INDEX_SIZE);
-    data_ptr += TE_CommandClass::BASE_ID_INDEX_SIZE;
-    *data_ptr = 0;
+    memcpy(current_ptr, go_base_object->goBaseIdIndex(), TE_CommandClass::BASE_ID_INDEX_SIZE);
+    current_ptr += TE_CommandClass::BASE_ID_INDEX_SIZE;
+    *current_ptr = 0;
 
     this->transmitFunction(downlink_data);
 }
@@ -77,18 +75,21 @@ void DEngineClass::processPutBaseData (char const *data_val)
 
 void DEngineClass::processPutBaseDataResponse (GoBaseClass *base_object_val, char const *data_val)
 {
-    char *downlink_data;
-    char *data_ptr;
 
     /* downlink */
     if (strlen(data_val) > TE_CommandClass::TE_DL_DATA_BUF_SIZE) {
         phwangAbendSI("DEngineClass::processPutBaseDataResponse", "buf_size", strlen(data_val));
     }
 
-    downlink_data = data_ptr = (char *) phwangMalloc(TE_CommandClass::TE_DL_DATA_BUF_SIZE + 4, MallocClass::DENGINE_PUT_BASE_DATA);
-    *data_ptr++ = TE_CommandClass::PUT_BASE_DATA_RESPONSE;
-    memcpy(data_ptr, base_object_val->roomIdIndex(), FT_CommandClass::ROOM_ID_INDEX_SIZE);
-    data_ptr += FT_CommandClass::ROOM_ID_INDEX_SIZE;
-    strcpy(data_ptr, data_val);
+    char *current_ptr;
+    char *downlink_data = current_ptr = (char *) phwangMalloc(TE_CommandClass::TE_DL_DATA_BUF_SIZE + 4, MallocClass::DENGINE_PUT_BASE_DATA);
+
+    *current_ptr++ = TE_CommandClass::PUT_BASE_DATA_RESPONSE;
+
+    memcpy(current_ptr, base_object_val->roomIdIndex(), FT_CommandClass::ROOM_ID_INDEX_SIZE);
+    current_ptr += FT_CommandClass::ROOM_ID_INDEX_SIZE;
+
+    strcpy(current_ptr, data_val);
+
     this->transmitFunction(downlink_data);
 }
