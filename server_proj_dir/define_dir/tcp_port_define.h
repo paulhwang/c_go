8b845f5e8edd3890666f77da0381b065ae08e03b
Paul Hwang <phwang@gmail.com>
2022-09-21 19:54:32 -0700

    port

--- server_proj_dir/define_dir/tcp_port_define.h
+++ server_proj_dir/define_dir/tcp_port_define.h
@@ -8,8 +8,8 @@
 
 class TcpPortDefine {
 public:
-    unsigned short const static FABRIC_IPHONE_PORT_NUMER  = 8001;
-    unsigned short const static FABRIC_ANDROID_PORT_NUMER = 8002;
+    unsigned short const static FABRIC_IPHONE_PORT_NUMER  = 8002;
+    unsigned short const static FABRIC_ANDROID_PORT_NUMER = 8001;
     unsigned short const static FABRIC_NODEJS_PORT_NUMER  = 8006;
     unsigned short const static FABRIC_THEME_PORT_NUMER   = 8007;
     unsigned short const static THEME_ENGINE_PORT_NUMER   = 8008;
