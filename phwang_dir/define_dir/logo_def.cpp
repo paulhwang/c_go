14ccf4b18ee1022d677bf73e12f64de639b51039
Paul Hwang <phwang@gmail.com>
2022-09-23 09:27:55 -0700

    logo

---
+++ phwang_dir/define_dir/logo_def.cpp
@@ -0,0 +1,9 @@
+/*
+  Copyrights reserved
+  Written by Paul Hwang
+  File name: logo_def.cpp
+*/
+
+#include "logo_def.h"
+
+char const *LOGO_DEF::PHWANG_LOGO = "phwang168";
---
+++ phwang_dir/define_dir/logo_def.h
@@ -0,0 +1,12 @@
+/*
+  Copyrights reserved
+  Written by Paul Hwang
+  File name: logo_def.h
+*/
+
+#pragma once
+
+class LOGO_DEF {
+public:
+    char const static *PHWANG_LOGO;
+};
