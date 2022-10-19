52ec903dfc35f2b2bb76caaa8dd94adb2f9f1a61
Paul Hwang <phwang@gmail.com>
2022-10-19 12:05:23 -0700

    R

--- phwang_dir/file_dir/file_read_class.cpp
+++ phwang_dir/file_dir/file_read_class.cpp
@@ -1,26 +1,26 @@
 /*
   Copyrights reserved
   Written by Paul Hwang
-  File name: file_read_class.cpp
+  File name: file_access_class.cpp
 */
 
 #include <errno.h>
 #include "../../phwang_dir/phwang.h"
 #include "file_read_class.h"
 
-FileReadClass::FileReadClass (void)
+FileAccessClass::FileAccessClass (void)
 {
-    phwangDebugS(true, "FileReadClass::FileReadClass", "start");
+    phwangDebugS(true, "FileAccessClass::FileAccessClass", "start");
 
     memset(this, 0, sizeof(*this));
 }
 
-FileReadClass::~FileReadClass (void)
+FileAccessClass::~FileAccessClass (void)
 {
-    phwangDebugS(true, "FileReadClass::~FileReadClass", "exit");
+    phwangDebugS(true, "FileAccessClass::~FileAccessClass", "exit");
 }
 
-void FileReadClass::readLine (char *buf_val, int *eof_val) {
+void FileAccessClass::readLine (char *buf_val, int *eof_val) {
     int index = 0;
 
     while (1) {
@@ -45,7 +45,7 @@ void FileReadClass::readLine (char *buf_val, int *eof_val) {
     *eof_val = 0;
 }
 
-int FileReadClass::readBytes (char *buf_val, int buf_size_val, int *eof_val)
+int FileAccessClass::readBytes (char *buf_val, int buf_size_val, int *eof_val)
 {
     int index = 0;
 
@@ -75,14 +75,14 @@ int FileReadClass::readBytes (char *buf_val, int buf_size_val, int *eof_val)
     return index;
 }
 
-int FileReadClass::openFile (char const *file_name_val, char const *mode_val)
+int FileAccessClass::openFile (char const *file_name_val, char const *mode_val)
 {
     int errno;
 
     this->fp_ = fopen(file_name_val, mode_val);
     if (this->fp_ == 0) {
         printf("***errno=%d\n", errno);
-        phwangLogitS("FileReadClass::openFile", "cannot open file");
+        phwangLogitS("FileAccessClass::openFile", "cannot open file");
         return -1;
     }
 
--- phwang_dir/file_dir/file_read_class.h
+++ phwang_dir/file_dir/file_read_class.h
@@ -1,19 +1,19 @@
 /*
   Copyrights reserved
   Written by Paul Hwang
-  File name: file_read_class.h
+  File name: file_access_class.h
 */
 
 #pragma once
 
-class FileReadClass {
+class FileAccessClass {
     FILE *fp_;
 
     void readNonemptyLine(char *buf_val, int *eof_val);
 
   public:
-    FileReadClass(void);
-    ~FileReadClass(void);
+    FileAccessClass(void);
+    ~FileAccessClass(void);
 
     int openFile(char const *filename_val, char const *mode_val);
     void readLine(char *buf_val, int *eof_val);
